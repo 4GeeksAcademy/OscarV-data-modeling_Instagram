c97505715bbf26053439d2b6c09b963ff10546f9
Oscar V <oscarvillalobosf@gmail.com>
2025-02-18 23:17:07 +0000

    First version of Instagram Diagrams py

--- src/models.py
+++ src/models.py
@@ -1,30 +1,49 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String,DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import datetime
+
+#python src/models.py
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname= Column(String(250), nullable=False)
+    email= Column(String(250), nullable=False)
+    
+class Post(Base):
+    __tablename__ = 'post'
+    id_post = Column(Integer, primary_key=True)
+    url_media = Column(String(250))
+    descripcion = Column(String(250))
+    like =Column(Integer)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
+class Comments(Base):
+    __tablename__ = 'comments'
+    id_com = Column(Integer, primary_key=True)
+    comments = Column(String(250))
+    id_post = Column(Integer, ForeignKey('post.id_post'))
+    post = relationship(Post)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Followers(Base):
+    __tablename__ = 'followers'
+    id_foll = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    id_follow = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
